0c75276358c53a3e1ef652d0d013fcdc9c1788d8
PiosX <piosso.pios@op.pl>
2022-05-11 03:28:33 +0200

    Prismic implementation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['standard'],
+  extends: ['standard', 'prettier'],
   globals: {
     IS_DEVELOPMENT: 'readonly'
   },
--- app.js
+++ app.js
@@ -1,27 +1,147 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+/* eslint-disable no-unused-vars */
+require("dotenv").config();
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+const express = require("express");
+const errorHandler = require("errorhandler");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const logger = require("morgan");
 
-app.get('/', (req, res) => {
-  res.render('pages/home')
-})
+const app = express();
+const port = 8004;
+const path = require("path");
 
-app.get('/about', (req, res) => {
-  res.render('pages/about')
-})
+const Prismic = require("@prismicio/client");
+const PrismicDOM = require("prismic-dom");
+const { Logger } = require("concurrently");
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
-})
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
+app.use(errorHandler());
 
-app.get('/detail/:', (req, res) => {
-  res.render('pages/detail')
-})
+// Initialize the prismic.io api
+const initApi = (req) => {
+	return Prismic.client(process.env.PRISMIC_ENDPOINT, {
+		accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+		req,
+	});
+};
+
+// Link Resolver
+const handleLinkResolver = (doc) => {
+	if (doc.type === "product") {
+		return `/detail/${doc.slug}`;
+	}
+
+	if (doc.type === "collections") {
+		return "/collections";
+	}
+
+	if (doc.type === "about") {
+		return `/about`;
+	}
+
+	return "/";
+};
+
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+	res.locals.Link = handleLinkResolver;
+
+	res.locals.Numbers = (index) => {
+		return index == 0
+			? "One"
+			: index == 1
+			? "Two"
+			: index == 2
+			? "Three"
+			: index == 3
+			? "Four"
+			: "";
+	};
+
+	res.locals.PrismicDOM = PrismicDOM;
+
+	next();
+});
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.locals.basedir = app.get("views");
+
+const handleRequest = async (api) => {
+	const meta = await api.getSingle("meta");
+	const navigation = await api.getSingle("navigation");
+	const preloader = await api.getSingle("preloader");
+
+	return {
+		meta,
+		navigation,
+		preloader,
+	};
+};
+
+app.get("/", async (req, res) => {
+	const api = await initApi(req);
+	const defaults = await handleRequest(api);
+	const home = await api.getSingle("home");
+
+	const { results: collections } = await api.query(
+		Prismic.Predicates.at("document.type", "collection"),
+		{ fetchLinks: "product.image" }
+	);
+
+	res.render("pages/home", {
+		...defaults,
+		collections,
+		home,
+	});
+});
+
+app.get("/about", async (req, res) => {
+	const api = await initApi(req);
+	const defaults = await handleRequest(api);
+	const about = await api.getSingle("about");
+
+	res.render("pages/about", {
+		...defaults,
+		about,
+	});
+});
+
+app.get("/detail/:uid", async (req, res) => {
+	const api = await initApi(req);
+	const defaults = await handleRequest(api);
+
+	const product = await api.getByUID("product", req.params.uid, {
+		fetchLinks: "collection.title",
+	});
+
+	res.render("pages/detail", {
+		...defaults,
+		product,
+	});
+});
+
+app.get("/collections", async (req, res) => {
+	const api = await initApi(req);
+	const defaults = await handleRequest(api);
+	const home = await api.getSingle("home");
+
+	const { results: collections } = await api.query(
+		Prismic.Predicates.at("document.type", "collection"),
+		{ fetchLinks: "product.image" }
+	);
+
+	res.render("pages/collections", {
+		...defaults,
+		collections,
+		home,
+	});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening at http://localhost:${port}`);
+});
