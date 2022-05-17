173837abd72e5435618bc03954f31df472fff19d
PiosX <piosso.pios@op.pl>
2022-05-17 21:54:44 +0200

    JavaScript Structure Implementation

--- app/index.js
+++ app/index.js
@@ -1,3 +1,71 @@
-import placeholder from 'images/angry.png'
+import each from "lodash/each";
 
-console.log(placeholder)
+import About from "pages/About";
+import Collections from "pages/Collections";
+import Detail from "pages/Detail";
+import Home from "pages/Home";
+
+class App {
+	constructor() {
+		this.createContent();
+		this.createPages();
+		this.addLinkListeners();
+	}
+
+	createContent() {
+		this.content = document.querySelector(".content");
+		this.template = this.content.getAttribute("data-template");
+	}
+
+	createPages() {
+		this.pages = {
+			about: new About(),
+			collections: new Collections(),
+			detail: new Detail(),
+			home: new Home(),
+		};
+
+		this.page = this.pages[this.template];
+		this.page.create();
+		this.page.show();
+	}
+
+	async onChange(url) {
+		await this.page.hide();
+
+		const res = await window.fetch(url);
+		if (res.status === 200) {
+			const html = await res.text();
+
+			const div = document.createElement("div");
+			div.innerHTML = html;
+
+			const divContent = div.querySelector(".content");
+			this.content.innerHTML = divContent.innerHTML;
+
+			this.template = divContent.getAttribute("data-template");
+			this.content.setAttribute("data-template", this.template);
+
+			this.page = this.pages[this.template];
+			this.page.create();
+			this.page.show();
+		} else {
+			console.error(`response status: ${res.status}`);
+		}
+	}
+
+	addLinkListeners() {
+		const links = document.querySelectorAll("a");
+
+		each(links, (link) => {
+			link.onclick = (event) => {
+				event.preventDefault();
+
+				const { href } = link;
+				this.onChange(href);
+			};
+		});
+	}
+}
+
+new App();
