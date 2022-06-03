27b2c2643ec3eba0537e7164cb43ed80f9aac7e0
PiosX <piosso.pios@op.pl>
2022-06-03 11:28:43 +0200

    Implementing vertex distortion

--- app/components/Canvas/Home/Media.js
+++ app/components/Canvas/Home/Media.js
@@ -1,8 +1,8 @@
 import { Mesh, Program } from "ogl";
 import GSAP from "gsap";
 
-import vertex from "shaders/plane-vertex.glsl";
-import fragment from "shaders/plane-fragment.glsl";
+import vertex from "shaders/home-vertex.glsl";
+import fragment from "shaders/home-fragment.glsl";
 
 export default class Media {
 	constructor({ element, geometry, gl, index, scene, sizes }) {
@@ -35,6 +35,10 @@ export default class Media {
 			vertex,
 			uniforms: {
 				uAlpha: { value: 0 },
+				uSpeed: { value: 0 },
+				uViewportSizes: {
+					value: [this.sizes.width, this.sizes.height],
+				},
 				tMap: { value: this.texture },
 			},
 		});
@@ -68,7 +72,7 @@ export default class Media {
 				value: 0,
 			},
 			{
-				value: 1,
+				value: 0.4,
 			}
 		);
 	}
@@ -114,9 +118,12 @@ export default class Media {
 		this.mesh.position.y = (this.sizes.height / 2) - (this.mesh.scale.y / 2) - (this.y  * this.sizes.height) + this.extra.y; // prettier-ignore
 	}
 
-	update(scroll) {
+	update(scroll, speed) {
 		if (!this.bounds) return;
+
 		this.updateX(scroll.x);
 		this.updateY(scroll.y);
+
+		this.program.uniforms.uSpeed.value = speed;
 	}
 }
